c7c7ed473b7038a94608b7984123f26a03e66dde
Fabio Garcia <fabio@ottava.co>
2019-03-07 00:13:14 -0500

    restructure

--- src/Ottava.node.js
+++ src/Ottava.node.js
@@ -1,10 +1,19 @@
-
+/** util **/
 import Debug from './Debug.js';
-
+/** crypto **/
 import Sha256 from './crypto/Sha256.js';
-
+/** abstractions **/
 import BasePort from './base/BasePort.js';
 import BaseStore from './base/BaseStore.js';
-
+/** implementaions **/
 import WSPort from './platform/node/WSPort.js';
 import FSStore from './platform/node/FSStore.js';
+
+let Ottava = {
+  Debug: Debug,
+  Sha256: Sha256,
+  Tranport: WSPort,
+  Storage: FSStore
+};
+
+export default Ottava;
--- src/Ottava.web.js
+++ src/Ottava.web.js
@@ -1,10 +1,19 @@
-
+/** util **/
 import Debug from './Debug.js';
-
+/** crypto **/
 import Sha256 from './crypto/Sha256.js';
-
+/** abstractions **/
 import BasePort from './base/BasePort.js';
 import BaseStore from './base/BaseStore.js';
-
+/** implementaions **/
 import WSPort from './platform/web/WSPort.js';
 import LSStore from './platform/web/LSStore.js';
+
+let Ottava = {
+  Debug: Debug,
+  Sha256: Sha256,
+  Tranport: WSPort,
+  Storage: LSStore
+};
+
+export default Ottava;
